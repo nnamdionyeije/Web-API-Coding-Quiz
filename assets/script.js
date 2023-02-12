221e0528c85162038661877bb1252749336350c0
nnamdionyeije <81874532+nnamdionyeije@users.noreply.github.com>
2023-02-12 01:14:41 -0500

    A few small changes and the beginning of the question section

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,23 @@ startButton.addEventListener("click", function(event) {
     mainPage.setAttribute("class", "title invisible");
 
 })
+
+let questionTest = {
+    position: 1,
+    mainText: "String Values must be enclosed within ______ when being assigned to variables.",
+    answer1: "commas",
+    answer2: "curly brackets",
+    answer3: "quotes",
+    answer4: "parenthesis"
+}
+
+let questionSet = question[10];
+
+let question = {
+    position,
+    mainText,
+    answer1,
+    answer2,
+    answer3,
+    answer4,
+}
