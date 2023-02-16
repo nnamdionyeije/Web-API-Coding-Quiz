80252b765bef4012f1d92dff47035b54d75a9344
nnamdionyeije <81874532+nnamdionyeije@users.noreply.github.com>
2023-02-16 18:52:11 -0500

    Cleaned up some small issues and worked on the High Scores page

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,32 @@
+// selectors for the main section content
 var startButton = document.querySelector(".start-button");
 var mainPage = document.querySelector(".title");
 var QuesPage = document.querySelector(".questions");
-// var buttonAction = document.querySelector(".buttons");
-var buttonAction = $('.all-buttons');
+var initPage = document.querySelector(".initials");
+var buttonAction = document.querySelector(".buttons");
 
-
-
-// selectors for the question text
+// selectors and related for the question text
 var questionTitle = document.querySelector("#QuesTitle");
 var button1 = document.querySelector("#buttonOne");
 var button2 = document.querySelector("#buttonTwo");
 var button3 = document.querySelector("#buttonThree");
 var button4 = document.querySelector("#buttonFour");
+var buttonArray = [button1, button2, button3, button4];
 
-var buttonArray = [button1, button2, button3, button4]
+// selectors for the correct/incorrect text
+var verdictSection = document.querySelector(".verdict-box");
+var verdictTextSpan = document.querySelector(".verdictText");
+var verdictTextInit = document.querySelector(".verdictTextInitials");
+var verdictValue = "";
+
+// time
+var timeSpan = document.querySelector(".time-span");
+var quizTime;
+var timer;
+// timeSpan.textContent = quizTime;
+
+//final score
+var scoreSpan = document.querySelector(".final-score");
 
 
 var currentQ = 1;
@@ -61,7 +74,9 @@ var questions = [
 
 function setQuestion() { //currentQ, buttonArray not sure if necessary to add those as inputs
     if (currentQ > questions.length) {
-        return;
+        QuesPage.setAttribute("class", "questions invisible");
+        initPage.setAttribute("class", "initials visible");
+        verdictTextInit.textContent = verdictValue;
     }
     
     questionTitle.textContent = questions[currentQ - 1].title;
@@ -69,26 +84,54 @@ function setQuestion() { //currentQ, buttonArray not sure if necessary to add th
     for (i = 0; i < buttonArray.length; i++) {
         buttonArray[i].textContent = questions[currentQ -1].choices[i];
     }
+
+    verdictTextSpan.textContent = verdictValue;
+
+    if (currentQ > 1) {
+        verdictSection.setAttribute("class", "verdict-box visible")
+    }
+}
+
+function setTime() {
+    timer = setInterval(function() {
+        quizTime--;
+        timeSpan.textContent = quizTime;
+        if (quizTime <= 0) {
+            QuesPage.setAttribute("class", "questions invisible");
+            initPage.setAttribute("class", "initials visible");
+            verdictTextInit.textContent = "Ran out of time!";
+            timeSpan.textContent = "0";
+            scoreSpan.textContent = "0";
+            clearInterval(timer);
+            //As things are this leaves the application with a short gap between input and effects
+            // A possible option is to delegate some of these responsibilities to the buttons
+        }
+        
+    }, 1000)
 }
 
 startButton.addEventListener("click", function(event) {
     mainPage.setAttribute("class", "title invisible");
     QuesPage.setAttribute("class", "questions visible");
+    quizTime = 75;
+    setTime();
     setQuestion();
 })
 
 //currently I have buttonAction set to the container for the buttons
 //ideally it would be set to the buttons directly
 //I was able to accomplish this using jQuery
-buttonAction.on("click", function(event) {
+buttonAction.addEventListener("click", function(event) {
     event.preventDefault();
 
     var element = event.target;
 
     if (element.matches('span')) {
         if (questions[currentQ-1].answer === element.textContent) {
-            console.log("correct answer")
-            // use this as a first step to check for correct answers
+            verdictValue = "Correct!"
+        } else {
+            verdictValue = "Incorrect";
+            quizTime -= 10; // look into how to make this function immediately subtract
         }
     } else if (element.matches('button')) {
         var newData = element.textContent.trim();
@@ -96,26 +139,37 @@ buttonAction.on("click", function(event) {
         switch (newData[0]) {
             case "1":
                 if (questions[currentQ-1].answer === button1.textContent) {
-                    console.log("Button Correct Answer")
-                    // use this as a first step to check for correct answers
+                            verdictValue = "Correct!"
+                } else {
+                    verdictValue = "Incorrect";
+                    quizTime -= 10;
                 }
                 break;
 
             case "2":
                 if (questions[currentQ-1].answer === button2.textContent) {
-                    console.log("Button Correct Answer")
+                    verdictValue = "Correct!"
+                } else {
+                    verdictValue = "Incorrect";
+                    quizTime -= 10;
                 }
                 break;
 
             case "3":
                 if (questions[currentQ-1].answer === button3.textContent) {
-                    console.log("Button Correct Answer")
+                    verdictValue = "Correct!"         
+                } else {
+                    verdictValue = "Incorrect";
+                    quizTime -= 10;
                 }
                 break;
 
             case "4":
                 if (questions[currentQ-1].answer === button4.textContent) {
-                    console.log("Button Correct Answer")
+                    verdictValue = "Correct!"             
+                } else {
+                    verdictValue = "Incorrect";
+                    quizTime -= 10;
                 }
                 break;
         }
@@ -123,8 +177,10 @@ buttonAction.on("click", function(event) {
     
     // console.log(element);
     // my current idea is to have
+
+    // set the upper border on a DIV below the question box
+    // then have the text in there
     currentQ++;
     setQuestion();
-    
 })
 
